8f562fc8994b650129fbc08283e44e70ea99fe00
Valerio Cocco <valerio.cocco@outlook.de>
2021-10-10 22:04:29 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
+// favicon for every page
+
 let head = document.getElementsByName('head');
 let favicon = document.createElement('link')
 favicon.setAttribute('rel', 'shortcut icon');
 favicon.setAttribute('type', 'image/x-icon');
 favicon.setAttribute('href', 'favicon.ico');
 head.item(0).appendChild(favicon)
+
+// favicon for every page
 
 function flyDrone() {
   let path = anime.path('path');
