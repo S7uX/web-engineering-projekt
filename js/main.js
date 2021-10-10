2c681be1612fed88929b5bd7bad0eff4fe4b30f3
Valerio Cocco <valerio.cocco@outlook.de>
2021-10-10 21:59:36 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-function flyDrone(){
+let head = document.getElementsByName('head');
+let favicon = document.createElement('link')
+favicon.setAttribute('rel', 'shortcut icon');
+favicon.setAttribute('type', 'image/x-icon');
+favicon.setAttribute('href', 'favicon.ico');
+head.item(0).appendChild(favicon)
+
+function flyDrone() {
   let path = anime.path('path');
 
   let easings = ['linear', 'easeInCubic', 'easeOutCubic', 'easeInOutCubic'];
