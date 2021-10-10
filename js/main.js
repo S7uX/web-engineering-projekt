de6688b88792429e6b5b7fc225214cab7298d5c5
Valerio <valerio.cocco@outlook.de>
2021-10-10 20:03:35 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+function flyDrone(){
+  let path = anime.path('path');
 
+  let easings = ['linear', 'easeInCubic', 'easeOutCubic', 'easeInOutCubic'];
+
+  anime({
+    targets: '.square',
+    translateX: path('x'),
+    translateY: path('y'),
+    easing: function (el, i) {
+      return easings[i];
+    },
+    duration: 13000,
+    loop: true
+  });
+}
