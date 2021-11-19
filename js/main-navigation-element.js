44dff02f0c489025631f0ee1b416f233af64a72e
Valerio <valerio.cocco@outlook.de>
2021-11-19 10:48:13 +0100

    update

--- js/main-navigation-element.js
+++ js/main-navigation-element.js
@@ -1,18 +1,19 @@
-class MainNavigationElement extends HTMLElement{
+class MainNavigationElement extends HTMLElement {
   constructor() {
     super();
   }
 
   connectedCallback() {
-    let start = document.createElement('div');
-    start.innerText = 'Start';
-    this.appendChild(start);
-    let products = document.createElement('div');
-    products.innerText = 'Produkte und Preise';
-    this.appendChild(products);
-    let Videos = document.createElement('div');
-    Videos.innerText = 'Start';
-    this.appendChild(Videos);
+    this.classList.add("main-navigation");
+    let text = ["Start", "Produkte und Preise", "Videos"];
+    let links = ["index.html", "products.html", "videos.html"]
+    for (let i = 0; i < links.length; i++) {
+      let navItem = document.createElement('a');
+      navItem.classList.add("nav-item");
+      navItem.innerText = text[i];
+      navItem.href = links[i];
+      this.appendChild(navItem);
+    }
   }
 
 }
