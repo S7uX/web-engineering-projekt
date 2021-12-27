52ce5ff5b68903c9fc1ac24c4dd15a53f0aa8bab
Valerio Cocco <valerio.cocco@outlook.de>
2021-12-27 01:57:49 +0100

    update

---
+++ js/drone.js
@@ -0,0 +1,16 @@
+function flyDrone() {
+    let path = anime.path('path');
+
+    let easings = ['linear', 'easeInCubic', 'easeOutCubic', 'easeInOutCubic'];
+
+    anime({
+        targets: '#drone',
+        translateX: path('x'),
+        translateY: path('y'),
+        easing: function (el, i) {
+            return easings[i];
+        },
+        duration: 13000,
+        loop: true
+    });
+}
--- js/main.js
+++ js/main.js
@@ -6,20 +6,3 @@ favicon.setAttribute('rel', 'shortcut icon');
 favicon.setAttribute('type', 'image/x-icon');
 favicon.setAttribute('href', 'favicon.ico');
 head.item(0).appendChild(favicon)
-
-function flyDrone() {
-  let path = anime.path('path');
-
-  let easings = ['linear', 'easeInCubic', 'easeOutCubic', 'easeInOutCubic'];
-
-  anime({
-    targets: '#drone',
-    translateX: path('x'),
-    translateY: path('y'),
-    easing: function (el, i) {
-      return easings[i];
-    },
-    duration: 13000,
-    loop: true
-  });
-}
