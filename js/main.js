6f869ef8269f43da2b641871b8e405fc1ffc5e36
Valerio Cocco <valerio.cocco@outlook.de>
2021-12-28 11:16:02 +0100

    update

--- js/main.js
+++ js/main.js
@@ -31,10 +31,10 @@ class MainNavigationElement extends HTMLElement {
 
     connectedCallback() {
         this.classList.add("main-navigation");
-        let text = ["Start", "Produkte und Preise", "Videos"];
-        let links = ["index.html", "products.html", "videos.html"]
+        const text = ["Start", "Produkte und Preise", "Videos"];
+        const links = ["index.html", "products.html", "videos.html"]
         for (let i = 0; i < links.length; i++) {
-            let navItem = document.createElement('a');
+            const navItem = document.createElement('a');
             navItem.classList.add("nav-item");
             navItem.innerText = text[i];
             navItem.href = links[i];
