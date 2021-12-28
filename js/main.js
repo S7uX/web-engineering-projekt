de86752c0a12622be449f01f3bd95ee4d4c84d0e
Valerio Cocco <valerio.cocco@outlook.de>
2021-12-28 18:58:49 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // favicon for every page
-let head = document.getElementsByTagName('head');
-let favicon = document.createElement('link')
+const head = document.getElementsByTagName('head');
+const favicon = document.createElement('link')
 favicon.setAttribute('rel', 'shortcut icon');
 favicon.setAttribute('type', 'image/x-icon');
 favicon.setAttribute('href', 'favicon.ico');
